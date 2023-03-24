6897b299df6f1d5310330ce213bfef2165629e2a
Galla Devendra <124939512+Team44Assistsu@users.noreply.github.com>
2023-03-24 17:56:05 -0400

    Updated priority java

--- src/main/java/com/acc/domain/Priority.java
+++ src/main/java/com/acc/domain/Priority.java
@@ -3,13 +3,13 @@ package com.acc.domain;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
