471f2a2525650bb9b3b600eb4f5d2ef18db0f94c
Galla Devendra <124939512+Team44Assistsu@users.noreply.github.com>
2023-03-24 21:02:59 -0400

    Create SortingService.java

---
+++ src/main/java/com/acc/service/SortingService.java
@@ -0,0 +1,15 @@
+package com.acc.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.acc.domain.Task;
+
+@Service
+public class SortingService {
+
+	public List<Task> sort(List<Task> tasks, String sortBy) {
+		return null;
+	}
+}
